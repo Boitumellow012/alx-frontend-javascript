61f654a818d73187f1c092e140d993caecc440e5
Boitumellow012 <brakobeloa@gmail.com>
2024-03-07 09:40:40 +0000

    classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  /**
+   * class Pricing
+   *
+   */
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('params should be numbers');
+  }
+}
